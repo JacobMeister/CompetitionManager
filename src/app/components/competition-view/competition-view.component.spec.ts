27bc0254d3c06f8e3c9bf416d89c3bf2f601c864
JacobMeister <maarten.jacobs15@gmail.com>
2018-08-21 00:28:54 +0200

    fixes

---
+++ src/app/components/competition-view/competition-view.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompetitionViewComponent } from './competition-view.component';
+
+describe('CompetitionViewComponent', () => {
+  let component: CompetitionViewComponent;
+  let fixture: ComponentFixture<CompetitionViewComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CompetitionViewComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompetitionViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show competition', () => {
+    expect(component.showCompetition(0)).toBe((0));
+  });
+});
---
+++ src/app/components/competition-view/competition-view.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-view',
+  templateUrl: './competition-view.component.html',
+  styleUrls: ['./competition-view.component.scss']
+})
+export class CompetitionViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  showCompetition(a){
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;  a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;    a = 0;
+   
+    return a;
+
+  }
+
+}
